e245489bb3a22c7c5b7444c48fc968cd6347af27
Ruan bertan <ruan.bertan@gmail.com>
2025-10-26 17:29:26 -0300

    fix: revisando conexao

--- public/comentario_proposta.php
+++ public/comentario_proposta.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
 // Redireciona de volta para os detalhes da proposta
-header("location: proposta_detalhe.php?id=" . intval($_GET['id']));
+header("location: proposta_detalhes.php?id=" . intval($_GET['id']));
 exit;
 }
 ?>
